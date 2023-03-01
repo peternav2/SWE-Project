bf5c6cfac13b700bb270e147c3c367fe4a666ba5
Peter Navarrete <peterabcs@gmail.com>
2023-02-28 21:58:35 -0500

    ternary operator addition

--- src/services/myFetch.ts
+++ src/services/myFetch.ts
@@ -8,7 +8,7 @@ export default function myFetch<T>(url: string, data?: any, method?: string ): P
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: data ? JSON.stringify(data) : undefined,
         }
         console.log("fetch");
         console.log(JSON.stringify(data));
@@ -20,8 +20,7 @@ export default function myFetch<T>(url: string, data?: any, method?: string ): P
         headers: { 
             'Content-Type': 'application/json',
         },
-        // body: data ? JSON.stringify(data) : undefined,
-        body: JSON.stringify(data),
+        body: data ? JSON.stringify(data) : undefined,
         
     };
     console.log("here2")
