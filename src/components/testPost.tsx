d84207b48035da178f2eba1b2c65f57015cfb939
Peter Navarrete <navarrep1@newpaltz.edu>
2023-03-03 18:33:09 -0500

    retrieve university array from database

--- src/components/testPost.tsx
+++ src/components/testPost.tsx
@@ -1,24 +1,18 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { Component, useEffect } from 'react'
 import myFetch from '../services/myFetch'
 import { add, DiningHall } from '../stores/DiningHall';
 import { addUniversity } from '../stores/University';
 import { MenuItem } from '../stores/MenuItem'
 import { University, getAllUniversities } from '../stores/University'
 export default function TestFetch() {
-  const [ unis, setUnis ] = useState<University[]>([]);
-  useEffect(() => {
-    getAllUniversities().then((data) => {
-      setUnis(data);
-    });
-  });
   async function runFetch() {
     console.log("before fetch");
-    //var universities: University[] = [];
-    // await getAllUniversities().then((data) => {
-    //   setUnis(data);
-    // })
-    console.log(unis);
-    
+    var universities: University[] = [];
+    await getAllUniversities().then((university) => {
+      universities = university;
+    })
+    console.log(universities);
+  
   }
 
   return (
