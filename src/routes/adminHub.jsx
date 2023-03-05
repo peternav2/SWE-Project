a9f98a62de3e4ed49c63ff3c21ee56128e1d070c
Frank <specificlaptop777@gmail.com>
2023-03-05 09:49:09 -0500

    add comments

--- src/routes/adminHub.jsx
+++ src/routes/adminHub.jsx
@@ -3,7 +3,8 @@ import RouterNav from '../components/routerNav';
 export default function AdminHub() {
   return (
     <main>
-      {/* <RouterNav /> */}
+      <RouterNav />
+       {/* Will show a list of dining halls with CRUD options for each dining hall*/}
       <h2>Admin Hub</h2>
     </main>
   );
