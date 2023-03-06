b70b0cb8018fe51ef2303b31d8b4936b80fb441c
RodoJML <rodojml@me.com>
2023-03-06 00:19:50 -0500

    Review form updated

--- src/components/reviewComponents/FeedbackForm.jsx
+++ src/components/reviewComponents/FeedbackForm.jsx
@@ -1,38 +1,60 @@
 import React from "react";
 
 class FeedbackForm extends React.Component {
+
     constructor(props) {
-      super(props);
-      this.state = {value: 'Please write your dish feedback'};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+        super(props);
+        this.state = {feedback: 'Please write your dish feedback', rating: 'nothing'};
+
+        this.handleChangeFeedback = this.handleChangeFeedback.bind(this);
+        this.handleChangeRating = this.handleChangeRating.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
+
+    handleChangeFeedback(event) {
+        this.setState({feedback: event.target.value });
     }
-  
+
+    handleChangeRating(event) {
+        this.setState({rating: event.target.value });
+    }
+
     handleSubmit(event) {
-      alert('Your feedback is: ' + this.state.value);
-      event.preventDefault();
+        alert('Your feedback is: ' + this.state.feedback + '\nYour rating is: ' + this.state.rating);
+        event.preventDefault();
     }
-  
+
     render() {
-      return (
-        <>
-        <p>{this.state.value}</p>
-        <form onSubmit={this.handleSubmit}>
-          <label>Feedback:</label>
-          <br/>
-          <textarea value={this.state.value} onChange={this.handleChange} />
-          <br/>
-          <input type="submit" value="Submit" />
-        </form>
-        </>
-      );
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+
+                    <label>Feedback:</label>
+                    <br />
+
+                    <textarea value={this.state.feedback} onChange={this.handleChangeFeedback} />
+                    <br />
+
+                    <label>Dish rating is:</label>
+                    <br />
+    
+                    <select value={this.state.rating} onChange={this.handleChangeRating}>
+                        <option value="1">Disatisfied</option>
+                        <option value="2">Somewhat Disatisfaction</option>
+                        <option value="3">Neutral</option>
+                        <option value="4">Somewhat Satisfied</option>
+                        <option value="5">Very Disatisfied</option>
+                    </select>
+
+                    <br />
+                    
+                    <input type="submit" value="Submit" />
+
+                </form>
+            </>
+        );
     }
-  }
+}
 
 export default FeedbackForm;
 
--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -1,12 +1,9 @@
 import FeedbackForm from "../components/reviewComponents/FeedbackForm"
-import RatingForm from "../components/reviewComponents/RatingForm"
 
 export default function ReviewPage() {
   return (
     <>
       <FeedbackForm />
-      <br/>
-      <RatingForm />
     </>
   )
 }
