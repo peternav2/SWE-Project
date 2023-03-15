5c1e93dae01a40c63b5c622225fafefa69fc436c
Peter Navarrete <navarrep1@newpaltz.edu>
2023-03-15 18:37:06 -0400

    test commit

--- src/stores/Day.ts
+++ src/stores/Day.ts
@@ -5,4 +5,3 @@ import { Dish } from "./Dish";
 export interface Day {
     date: CalendarDate
 }
-// for now the Day object will only have a calendarDate field, but we can add more fields later if we want to eg an optional Theme of the day or something like that
