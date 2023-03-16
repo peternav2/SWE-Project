b7d746bcdb3156d26fd19f28a6d78febdcce6945
peternav2 <91702295+peternav2@users.noreply.github.com>
2023-03-15 21:29:00 -0400

    Revert "Development peter"

--- src/components/testPost.tsx
+++ src/components/testPost.tsx
@@ -1,157 +1,36 @@
 import React, { Component, useEffect } from 'react'
 import myFetch from '../services/myFetch'
-import { addDiningHallToUniversity, deleteDiningHallFromUniversity, DiningHall } from '../stores/DiningHall';
-import { addUniversity } from '../stores/University';
-import { Dish } from '../stores/Dish'
-import { University, getAllUniversities } from '../stores/University'
-import { ObjectId } from 'mongodb';
-import mongodb from 'mongodb';
-import { User, addUser, getUserByUsernamePassword, getUserById, deleteUser } from '../stores/User';
-import { addMenuItem, getMenuItemsBasedByDate, MenuItem, getMenuItemsBasedByDiningHall, getMenuItemsByMealTypeByDate, deleteMenuItem, getMenuItemById } from '../stores/MenuItem';
-import { addReviewToMenuItem, deleteReviewFromMenuItem, getReviewsByMenuItem, Review } from '../stores/Review';
+import { MenuItem } from '../stores/MenuItem'
+import { University } from '../stores/University'
+export default function testFetch() {
 
 
-export default function TestFetch() {
 
-  
   async function runFetch() {
-    var unis: University[] = [];
-    await getAllUniversities().then((res) => {
-      unis = res;
-      // console.log(res);
-      // console.log("--------------------");
-      // console.log(unis);
-    })
-
-    var testUser: User = {
-      username: "Peters Test User",
-      password: "Test Password",
-      isStudent: true,
-      universityId: unis[1]._id,
-    }
-    var testDining: DiningHall = {
-      name: "Test Dining Hall",
-    }
-    
-    if (unis[1]._id){ // if statement to prevent 'error cannot use type undefined | ObjectId'
-      await addDiningHallToUniversity(testDining, unis[1]._id).then((res) => {
-        console.log("DINING HALL ADDED TO UNI");
-        
-        console.log(res);
-        testDining._id = res._id;
-      })
-    }
-
-    await deleteDiningHallFromUniversity(testDining._id, unis[1]._id).then((res) => {
-      console.log("DINING HALL DELETED FROM UNI");
-      console.log(res);
-      
-    });
-    await addUser(testUser).then((res) => {
-      testUser._id = res._id;
-    })
-    console.log("LOOK HEREERERER");
-    
-    console.log(testDining._id);
-
-
-
-    var testDish: Dish = {
-      name: "Test Dish",
+    console.log("before fetch");
+    const menuItem: MenuItem = {
+      name: "University of Toronto",
       cal: 100,
-      description: "Test Description",
-      allergens:  ["Test Allergen",
-                  "Test Allergen 2"],
-      ingredients: ["Test Ingredient",
-                    "Test Ingredient 2",
-                    "Test Ingredient 3"],
-      reviews: [],
-      diningHallId: unis[1].diningHalls[0]._id,
+      university: {
+        name: "University of Toronto",
+        diningHalls: [
+          {name: "St. George"},
+        ]
+      },
+
     }
-    console.log("hello");
+    await myFetch('menuitems', { menuItem }).then((res) => {
+      console.log("res", res);
+    }).catch((err) => {
+      console.log("err", err);
+  });
+    console.log("after fetch");
     
-    var testMenuItem: MenuItem = {
-      mealType: "Breakfast",
-      dish: testDish,
-      date: {year: 2023, month: 1, day: 1}
-    }
-
-    await addMenuItem(testMenuItem).then((res) => {
-      testMenuItem._id = res._id;
-    })
-
-    await getMenuItemById(testMenuItem._id).then((res) => {
-      console.log("GET MENU ITEM WITH ID HERE");
-      console.log(res);  
-    })
-    console.log("LOOK HERE TEST MENU ITEM IS BELOW");
-    
-    console.log(testMenuItem);
-    
-    await getUserByUsernamePassword(testUser.username, testUser.password).then((res) => {
-      console.log("USER IS GOTTEN BELOW BY PASSWORD");
-      console.log(res);
-    })
-
-    await getUserById(testUser._id).then((res) => {
-      console.log("USER IS GOTTEN BELOW BY ID");
-      console.log(res);
-    })
-
-
-    var review: Review = {
-      username: "Test User",
-      user_Id: testUser._id,
-      rating: 5,
-      comment: "Test Comment",
-    }
-
-    // await deleteUser(testUser.username, testUser.password).then((res) => {
-    //   console.log("USER IS DELETED BELOW");
-    //   console.log(res);
-    // })
-
-    await getMenuItemsBasedByDate(testMenuItem.date, testMenuItem.dish.diningHallId).then((res) => {
-      console.log("MENU ITEMS FROM DATE ARE GOTTEN BELOW");
-      console.log(res);
-    })
-
-    await getMenuItemsBasedByDiningHall(unis[1].diningHalls[0]._id).then((res) => {
-      console.log("MENU ITEMS FROM DINING HALL ARE GOTTEN BELOW");
-      console.log(res);
-    })
-
-    await getMenuItemsByMealTypeByDate(testMenuItem.date, "Breakfast", unis[1].diningHalls[0]._id).then((res) => {
-      console.log("MENU ITEMS FROM DATE AND MEAL TYPE ARE GOTTEN BELOW");
-      console.log(res);
-    })
-
-
-    console.log(testDining);
-
-    await addReviewToMenuItem(review, testMenuItem._id).then((res) => {
-      console.log("REVIEW IS ADDED BELOW");
-      console.log(res);
-    });
-
-    await getReviewsByMenuItem(testMenuItem._id).then((res) => {
-      console.log("REVIEWS ARE GOTTEN BELOW");
-      console.log(res);
-    })
-
-    await deleteReviewFromMenuItem(review, testMenuItem._id).then((res) => {
-      console.log("REVIEW IS DELETED BELOW");
-      console.log(res);
-    });
-    await deleteMenuItem(testMenuItem._id).then((res) => {
-      console.log("MENU ITEM IS DELETED BELOW");
-      console.log(res);
-    });
   }
-
 
   return (
     <div>
         <button onClick={runFetch} >Test Fetch</button>
     </div>
-  )}
+  )
+}
--- src/stores/MenuItem.ts
+++ src/stores/MenuItem.ts
@@ -1,80 +1,8 @@
-import { ObjectId } from "mongodb";
-import myFetch from "../services/myFetch";
-import { CalendarDate } from "./CalendarDate";
-import { Dish } from "./Dish";
+import { University } from "./University";
 
 export interface MenuItem {
-    mealType: string; // breakfast, lunch, dinner
-    dish: Dish;
-    date: CalendarDate; // xxxx-xx-xx format eg {year: 2020, month: 10, day: 10};
-    _id?: ObjectId;
-}
-
-/**
- * 
- * @param menuItem : MenuItem
- * @returns Promise<MenuItem> : a promise that resolves to the menu item that was just added to the database with the _id field added
- */
-export async function addMenuItem(menuItem: MenuItem): Promise<MenuItem> {
-    return await myFetch<MenuItem>(`menuitem`, menuItem)
-}
-
-/**
- * 
- * @param date : CalendarDate: {year: number, month: number, day: number}
- * @param diningHallId : ObjectId
- * @returns Promise<MenuItem[]> : a promise that resolves to an array of menu items that have the date you are requesting
- */
-export async function getMenuItemsBasedByDate(date: CalendarDate, diningHallId?: ObjectId): Promise<MenuItem[]> { 
-    return await myFetch<MenuItem[]>(`menuitem/${date.year}/${date.month}/${date.day}/${diningHallId}`)
-}
-
-/**
- * 
- * @param menuItemId : ObjectId
- * @returns Promise<MenuItem> : a promise that resolves to the menu item you are requesting from its _id
- */
-export async function getMenuItemById(menuItemId?: ObjectId): Promise<MenuItem> {
-    return await myFetch<MenuItem>(`menuitem/${menuItemId}`)
-}
-
-/**
- * 
- * @param diningHallId : ObjectId
- * @returns Promise<MenuItem[]> : a promise that resolves to an array of menu items that have the dining hall id you are requesting
- */
-export async function getMenuItemsBasedByDiningHall(diningHallId?: ObjectId): Promise<MenuItem[]> { 
-    return await myFetch<MenuItem[]>(`menuitem/get/diningHall/${diningHallId}`)
-}
-
-/**
- * 
- * @param date : CalendarDate: {year: number, month: number, day: number}
- * @param mealType : string: "breakfast", "lunch", "dinner", "lateNight"
- * @param diningHallId : ObjectId
- * @returns Promise<MenuItem[]> : a promise that resolves to an array of menu items that have the meal type and date you are requesting
- */
-export async function getMenuItemsByMealTypeByDate(date: CalendarDate, mealType: string, diningHallId?: ObjectId): Promise<MenuItem[]> {
-    return await myFetch<MenuItem[]>(`menuitem/${date.year}/${date.month}/${date.day}/${mealType}/${diningHallId}`)
-}
-
-/**
- * 
- * @param dishName : string
- * @param diningHallId : ObjectId
- * @returns Promise<MenuItem[]> : a promise that resolves to an array of menu items that have the dish name you are requesting
- */
-
-export async function getMenuItemsByDishName(dishName: string, diningHallId?: ObjectId): Promise<MenuItem[]> { // not implemented
-    return await myFetch<MenuItem[]>(`menuitem/get/dish/${dishName}/${diningHallId}`)
-}
-
-/**
- * 
- * @param menuItemId : ObjectId 
- * @returns Promise<any> : a promise that resolves to an object with details about the deletion.
- */
-
-export async function deleteMenuItem(menuItemId?: ObjectId): Promise<any> { 
-    return await myFetch<any>(`menuitem/${menuItemId}`, null, "DELETE");
+    _id?: string;
+    name: string;
+    cal: number;
+    university: University
 }
