005c4680fe8ce631c1a108ccd4b1a34a9d9238c6
Peter Navarrete <navarrep1@newpaltz.edu>
2023-03-23 17:42:44 -0400

    delete jsx files

--- src/components/UniversityFoodReviewBox.jsx
+++
@@ -1,9 +0,0 @@
-//This is a component for displaying reviews of individual dining hall foods.
-//which will be gathered from user's reports. 
-export default function RouterNav() {
-    return (
-        <div>
-
-        </div>
-    );
-}
