442b7faf2d237d7909cc8870d94e18dbcd0d7027
RodoJML <rodojml@me.com>
2023-03-26 22:23:55 -0400

    admin home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 import {User} from "./stores/User";
-import {Outlet, Route, Routes, useOutletContext} from "react-router-dom";
+import TestFetch from "./components/testPost";
+import {Outlet, useOutletContext} from "react-router-dom";
 
 type ContextType = [user: User, setUser: (user: User) => void];
 function App() {
+
   const [userContext, setUserContext] = useState<User | null>(null);
+
     return (
     <div>
+      
       <Outlet context={[userContext, setUserContext]}/>
+      <TestFetch/>
     {/*
          the above Outlet is where our app is gonna render and it is through this element which our app will render
          and have access to the user store through Context
--- src/pages/adminHome.tsx
+++ src/pages/adminHome.tsx
@@ -1,1 +1,12 @@
-export {}
+import React, { Component } from 'react'
+import { getUserByUsernamePassword, type User } from '../stores/User';
+
+export default function adminHome() {
+
+    let currentUser: User = getUserByUsernamePassword("RodoJML", "1234");
+
+    return (
+      <div>{ currentUser.isStudent }</div>
+    )
+  
+}
