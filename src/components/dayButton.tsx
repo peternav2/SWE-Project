609cec05fcebb35c596aecfb8666e53bff7ee6c4
Peter Navarrete <navarrep1@newpaltz.edu>
2023-03-31 15:49:04 -0400

    petty style

--- src/components/dayButton.tsx
+++ src/components/dayButton.tsx
@@ -6,7 +6,7 @@ export default function DayButton({userType, day, month, year, diningHallId}:
 { userType: string, day: number, month: number, year: number, diningHallId?: string }) {
     return (
         <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded ">
                 <Link to={`${userType}/day/menu/${diningHallId}/month/day/year`}>
                     {new Date(year-1,month-1).toLocaleString('default', {month: 'long'})
                     + " " + day
