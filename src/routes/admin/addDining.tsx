856ca21eb706ec6d5fc9b1945decfc64daf2eb91
RodoJML <rodojml@me.com>
2023-04-02 01:09:28 -0400

    Adding Styling

--- src/routes/admin/addDining.tsx
+++ src/routes/admin/addDining.tsx
@@ -7,7 +7,6 @@ export async function loader({ params }: any) {
 }
 
 export async function action({ request, params }: any) {
-
     const formData = await request.formData();
     const diningHall: DiningHall = { name: formData.get('diningName') }
     const universityId = params.universityId as ObjectId;
@@ -16,42 +15,41 @@ export async function action({ request, params }: any) {
     return redirect(`/admin/university/${params.universityId}`);
 }
 
-
 export default function AddDining() {
-
     const universityId = useLoaderData() as string;
     const navigate = useNavigate();
 
     return (
-        <div>
-            <h1>Dining Details</h1>
+        <div className={div1style}>
+            <div className={div2style}>
+                <Form method="post">
+                    <h2 className={diningDetailTitle}>Dining Details</h2>
 
-            <Form method="post">
-                <p>
-                    <span>Name: </span>
-                    <input
-                        placeholder="First"
-                        type="text"
-                        name="diningName"
-                    />
-                </p>
-                <p>
-                    <button
-                        type="submit"
-                        className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
-                    >
+                    <label className={labelStyle}>Name</label>
+
+                    <div className={inputWrapperStyle}>
+                        <input className={inputStyle} placeholder="First" type="text" name="diningName" />
+                    </div>
+
+                    <button type="submit" className={saveButtonStyle}>
                         Save
                     </button>
 
-                    <button
-                        type="button"
-                        onClick={() => { navigate(-1); }}
-                        className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-gray-500 hover:bg-gray-700"
-                    >
+                    <button type="button" className={cancelButtonStyle} onClick={() => { navigate(-1) }}>
                         Cancel
                     </button>
-                </p>
-            </Form>
+                </Form>
+            </div>
         </div>
     )
 }
+
+// Styles
+const div1style = "max-w-sm w-full lg:max-w-full lg:flex"
+const div2style = "my-2 border-t border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+const labelStyle = "block text-sm font-medium leading-6 text-gray-900"
+const inputWrapperStyle = "flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+const inputStyle = "block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+const saveButtonStyle = "py-2 px-4 mx-1 my-3 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+const cancelButtonStyle = "py-2 px-4 mx-1 my-3 font-semibold rounded-lg shadow-md text-white bg-gray-500 hover:bg-gray-700"
+const diningDetailTitle = "my-2 text-base font-semibold leading-7 text-gray-900"
--- src/routes/admin/adminRoot.tsx
+++ src/routes/admin/adminRoot.tsx
@@ -12,9 +12,11 @@ export default function AdminHome() {
 
   return (
     <>
-      <div>
-        <h1>{university.name}</h1>
-        <br />
+      <div className="mx-2">
+        <h1 className={universityTitleStyle}>
+          {university.name}
+        </h1>
+
         <ul>
           {university.diningHalls.map((hall?) => (
             <li key={hall._id?.toString()}>
@@ -23,7 +25,7 @@ export default function AdminHome() {
                 className={({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}>
 
                 <Link to={`/admin/university/${university?._id}/dining/${hall._id}`}>
-                  <div className="py-2 px-4 max-w-sm my-3 font-semibold rounded-lg shadow-md text-white bg-yellow-500 hover:bg-yellow-700">
+                  <div className={diningButtonsStyle}>
                     {hall.name}
                   </div>
                 </Link>
@@ -33,21 +35,23 @@ export default function AdminHome() {
           ))}
         </ul>
 
-        <br />
-
-        <button>
-          <Link to={`/admin/university/${university?._id}/addDining`}>
-            New Dining Hall
+        <button type="submit" className={newDiningButtonStyle}>
+          <Link className="flex" to={`/admin/university/${university?._id}/addDining`}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+            </svg>
+            <p>New Dining Hall</p>
           </Link>
         </button>
-
-        <br /><br />
 
         <Outlet />
 
       </div>
     </>
-
   )
-
 }
+
+// Styles
+const diningButtonsStyle = "py-2 px-4 max-w-sm my-3 font-semibold rounded-lg shadow-md text-white bg-yellow-500 hover:bg-yellow-700"
+const newDiningButtonStyle = "rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+const universityTitleStyle = "block font-sans text-4xl font-semibold leading-[1.3] tracking-normal text-inherit antialiased"
