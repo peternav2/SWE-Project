2945f55940c33ec9287e3405f5f6b8e58873f1d9
RodoJML <rodojml@me.com>
2023-04-08 13:18:55 -0400

    create menu update

--- src/routes/admin/createMenu.tsx
+++ src/routes/admin/createMenu.tsx
@@ -1,14 +1,50 @@
-import { Form, redirect, useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { Form, Link, Outlet, redirect, useLoaderData, useNavigate, useParams } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import { useEffect, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import type { MenuItem } from "../../stores/MenuItem";
-import { getMenuItemsBasedByDate } from "../../stores/MenuItem";
+import { getMenuItemsBasedByDate, addMenuItem } from "../../stores/MenuItem";
 import type { CalendarDate } from "../../stores/CalendarDate";
 import MenuItemForm from "./menuItemForm";
+import { Dish } from "../../stores/Dish";
+import { Review } from "../../stores/Review";
 
-export async function action() {
-    // here we will submit changes 
+
+export async function action({ request, params }: any) {
+
+    // Update function to be placed here... 
+    const formData = await request.formData();
+    const updates = Object.fromEntries(formData);
+
+    if (updates.newAlergen == undefined) {
+        
+        const newSubmitItem = {
+            mealType: params.mealType,
+            
+            dish: {
+                name: updates.dishName,
+                cal: updates.calories as number,
+                allergens: [] as string[],
+                ingredients: [] as string[],
+                reviews: [] as Review[],
+                description: updates.description,
+                diningHallId: params.diningHallId
+            } as Dish,
+
+            date: {
+                year: +params.year,
+                month: +params.month,
+                day: +params.day,
+            } as CalendarDate
+        } as MenuItem;
+
+        await addMenuItem(newSubmitItem);
+
+    } else {
+        // Edit existing menu item
+    }
+
+    return redirect(`/admin/university/${params.universityId}/dininghall/${params.diningHallId}/createmenu/${params.month}/${params.day}/${params.year}`);
 }
 
 export async function loader({ params }: any) {
@@ -22,17 +58,21 @@ export async function loader({ params }: any) {
 
 export default function CreateMenu() {
 
+    const navigate = useNavigate();
+    const menuItems = useLoaderData() as MenuItem[];
     const [startDate, setStartDate] = useState(new Date());
     const [mealType, setMealType] = useState("All Day");
     const [filteredMenus, setFilteredMenus] = useState([] as MenuItem[]);
+    const [addingNewMenuItem, setaddingNewMenuItem] = useState(false);
 
     const universityId = useParams().universityId;
     const diningHallId = useParams().diningHallId;
-    const menuItems = useLoaderData() as MenuItem[];
-    const navigate = useNavigate();
+    const day = useParams().day;
+    const month = useParams().month;
+    const year = useParams().year;
 
     useEffect(() => {
-        if (mealType === "All Day") {
+        if (mealType == "All Day") {
             setFilteredMenus(menuItems);
         } else {
             setFilteredMenus(menuItems.filter((item) => { return item.mealType === mealType; }));
@@ -41,57 +81,87 @@ export default function CreateMenu() {
 
     return (
         <div>
-            <h1> Create a New Menu </h1>
+            <h1 className={title}>Create a New Menu</h1>
 
             <DatePicker className={dateButton} selected={startDate}
                 onChange={(date) => {
                     setStartDate(date as Date);
                     const dateIn = date as Date;
                     navigate(`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${dateIn.getMonth() + 1}/${dateIn.getDate()}/${dateIn.getFullYear()}`);
-                }}
-            />
+                }} />
 
             <ul className="flex">
                 <li className="flex-1 mr-2">
-                    <a className={mealType === "Breakfast" ? activeButton : navButton}
-                    onClick={() => setMealType("Breakfast")}>Breakfast</a>
+                    <a
+                        //to={`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${month}/${day}/${year}/`}
+                        className={mealType === "Breakfast" ? activeButton : navButton}
+                        onClick={() => setMealType("Breakfast")}>
+                        Breakfast
+                    </a>
                 </li>
                 <li className="flex-1 mr-2">
-                    <a className={mealType === "Lunch" ? activeButton : navButton}
-                    onClick={() => setMealType("Lunch")}>Lunch</a>
+                    <a
+                        //to={`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${month}/${day}/${year}/`}
+                        className={mealType === "Lunch" ? activeButton : navButton}
+                        onClick={() => setMealType("Lunch")}>
+                        Lunch
+                    </a>
                 </li>
                 <li className="flex-1 mr-2">
-                    <a className={mealType === "Dinner" ? activeButton : navButton}
-                    onClick={() => setMealType("Dinner")}>Dinner</a>
+                    <a
+                        //to={`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${month}/${day}/${year}/`}
+                        className={mealType === "Dinner" ? activeButton : navButton}
+                        onClick={() => setMealType("Dinner")}>
+                        Dinner
+                    </a>
                 </li>
                 <li className="flex-1 mr-2">
-                    <a className={mealType === "Late Night" ? activeButton : navButton}
-                    onClick={() => setMealType("Late Night")}>Late Night</a>
+                    <a
+                        //to={`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${month}/${day}/${year}/`}
+                        className={mealType === "Late Night" ? activeButton : navButton}
+                        onClick={() => setMealType("Late Night")}>
+                        Late Night
+                    </a>
                 </li>
                 <li className="flex-1 mr-2">
-                    <a className={mealType === "All Day" ? activeButton : navButton}
-                    onClick={() => setMealType("All Day")}>All Day</a>
+                    <a
+                        //to={`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${month}/${day}/${year}/`}
+                        className={mealType === "All Day" ? activeButton : navButton}
+                        onClick={() => setMealType("All Day")}>
+                        All Day
+                    </a>
                 </li>
             </ul>
 
-            {filteredMenus.map((menuItem) => (
-                <div key={menuItem._id?.toString()}>
-                    <p>{menuItem.dish.name}</p>
+            {
+                (mealType != "All Day") &&
+                (<Link
+                    to={`/admin/university/${universityId}/dininghall/${diningHallId}/createmenu/${month}/${day}/${year}/new/${mealType}`}
+                    className={gactiveButton}
+                    onClick={() => setaddingNewMenuItem(!addingNewMenuItem)}>
+                    Add New Menu Item
+                </Link>)
+            }
+
+            <Outlet/>
+
+            {filteredMenus.slice(0).map((menuItem) => (
+                <div className="mx-2 my-2" key={menuItem._id?.toString()}>
                     <MenuItemForm item={menuItem}></MenuItemForm>
                 </div>
             ))}
-
-            <button className={activeButton} disabled>Add New Menu Item</button>
 
         </div>
     )
 }
 
 // Styles
-const activeButton = "text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
-const dateButton = "text-center block border border-yellow-500 rounded py-2 px-4 bg-yellow-500 hover:bg-yellow-700 text-white"
-const navButton = "text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4"
+const activeButton = "mx-2 my-2 text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
+const gactiveButton = "mx-2 my-2 text-center block border border-green-500 rounded py-2 px-4 bg-green-500 hover:bg-green-700 text-white"
+const dateButton = " mx-2 my-2 text-center block border border-yellow-500 rounded py-2 px-4 bg-yellow-500 hover:bg-yellow-700 text-white"
+const navButton = "mx-2 my-2 text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4"
 const DisabledButton = "text-center block py-2 px-4 text-gray-400 cursor-not-allowed"
+const title = "mx-2 my-2 inline-block text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight dark:text-black-200"
 
 
 
