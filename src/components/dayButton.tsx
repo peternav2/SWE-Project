c69b9c76322f61396516fe7d909330fd1ef957eb
Peter Navarrete <navarrep1@newpaltz.edu>
2023-04-10 13:50:10 -0400

    buttons now actually work

--- src/components/dayButton.tsx
+++ src/components/dayButton.tsx
@@ -1,13 +1,22 @@
 import {ObjectId} from "mongodb";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import React from "react";
 
 export default function DayButton({userType, day, month, year, diningHallId}:
 { userType: string, day: number, month: number, year: number, diningHallId?: string }) {
+
+    const navigate = useNavigate();
+
+    function toDay() {
+      navigate(`/student/day/menu/${diningHallId}/${month}/${day}/${year}`)
+    }
+
+
+
     return (
         <div>
-            <button className="bg-blue-600 hover:bg-blue-800 text-white font-bold text-2xl py-6 px-5 rounded ">
-                <Link to={`${userType}/day/menu/${diningHallId}/month/day/year`} >
+            <button onClick={toDay} className="bg-blue-600 hover:bg-blue-800 text-white font-bold text-2xl py-6 px-5 rounded ">
+                <Link to={`/`} >
                     {new Date(year-1,month-1)
                       .toLocaleString('default', {month: 'long'})
                     }
