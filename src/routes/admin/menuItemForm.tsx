8ee34eda40d0dc13e677886e15d6df73584a125e
RodoJML <rodojml@me.com>
2023-04-21 14:42:31 -0400

    Styling added to menuItemform and createmenu

--- src/routes/admin/menuItemForm.tsx
+++ src/routes/admin/menuItemForm.tsx
@@ -73,7 +73,7 @@ export default function MenuItemForm({ item = {} as MenuItem }) {
     }
 
     return (
-        <Form className={div1style} method="post" id="menuItem-form">
+        <Form className={divstyle1} method="post" id="menuItem-form">
             <div className="mx-2 my-2">
                 <p>
                     <input className={dishNames} type="text" name="dishName" defaultValue={item?.dish?.name}
@@ -85,8 +85,8 @@ export default function MenuItemForm({ item = {} as MenuItem }) {
                 </p>
 
                 <p>
-                    <label>Cal </label>
-                    <input type="number" name="calories" defaultValue={item?.dish?.cal}
+                    <label>Cal <strong className="text-red-600">{calories}</strong> </label>
+                    <input type="range" name="calories" value={calories} max="1000"
                         className={inputsMargin} onChange={(event) => {
                             setValueChanged(true)
                             setCalories(+event.target.value)
@@ -96,7 +96,7 @@ export default function MenuItemForm({ item = {} as MenuItem }) {
                 <p>
                     <label>Description: </label>
                     <input type="text" name="description" defaultValue={item?.dish?.description}
-                        className={inputsMargin} onChange={(event) => {
+                        className={inputsMargin + " text-gray-400"} onChange={(event) => {
                             setValueChanged(true)
                             setDescription(event.target.value as string)
                         }} />
@@ -180,14 +180,16 @@ export default function MenuItemForm({ item = {} as MenuItem }) {
                             </Link>
                         </div>
                     }
+
+                    {!newItem &&
+                        <Link to={`/admin/university/${params.universityId}/dininghall/${params.diningHallId}/createmenu/${params.month}/${params.day}/${params.year}/`}
+                            onClick={() => (deleteMenuItem(item._id))} className={activeRedButton}>
+                            Delete
+                        </Link>
+                    }
                 </div>
 
-                {!newItem &&
-                    <Link to={`/admin/university/${params.universityId}/dininghall/${params.diningHallId}/createmenu/${params.month}/${params.day}/${params.year}/`}
-                        onClick={() => (deleteMenuItem(item._id))} className={activeRedButton}>
-                        Delete
-                    </Link>
-                }
+
 
             </div>
         </Form>
@@ -205,7 +207,8 @@ const inputsMargin = "my-1"
 const dishNames = "text-2xl font-bold"
 const activeButton = "mx-2 my-2 text-center flex border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
 const activeRedButton = "mx-2 my-2 text-center flex border border-red-500 rounded py-2 px-4 bg-red-500 hover:bg-red-700 text-white"
-const div1style = "border-t border-r border-b border-l border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r lg:rounded-l p-4 flex flex-col justify-between leading-normal"
+// const div1style = "border-t border-r border-b border-l border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r lg:rounded-l p-4 flex flex-col justify-between leading-normal"
+const divstyle1 = "bg-menu-pattern1 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
 const trashIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
 </svg>
