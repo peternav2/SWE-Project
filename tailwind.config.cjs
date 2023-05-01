1e1917d3bc955f6ea4972dfc49619b9f59a0eeac
Peter Navarrete <peterabcs@gmail.com>
2023-05-01 12:37:15 -0400

    comment out typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       }
     },
   },
-  plugins: [require("@tailwindcss/typography"),require("daisyui")],
+  // plugins: [require("@tailwindcss/typography"),require("daisyui")],
   daisyui: {
     styled: true,
     themes: ["corporate", "dark"],
