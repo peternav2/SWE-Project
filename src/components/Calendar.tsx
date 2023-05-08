9245c5821eae1d361b7c2113dba7786a3f03fa09
Peter Navarrete <navarrep1@newpaltz.edu>
2023-05-08 16:23:45 -0400

    br

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -134,6 +134,7 @@ export default function Calendar() {
           })
         }
       </div>
+      <br></br>
       <button onClick={toDHalls} className={"w-full text-white border-blue-1000  bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center relative mb-4"}>
         Go To Dining Halls
       </button>
